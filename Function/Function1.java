efe5d5230a20e03c3eb8fcf43b1ea6a9032f038e
Vishalkumar8757 <vishalkumaroppo8757@gmail.com>
2026-02-10 04:13:29 +0530

    onlineclasses

--- Function/Function1.java
+++ Function/Function1.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 class  Function1{
 public static void main(String args[]){
-      Scanner sc = new Scanner(System.in);
+      Scanner s = new Scanner(System.in);
       
       System.out.print("Enter first number :");
-      int a = sc.nextInt();
+      int a = s.nextInt();
 
       System.out.print("Enter second number :");
-      int b = sc.nextInt();
+      int b = s.nextInt();
 
       System.out.print("Enter third number :");
-      int c = sc.nextInt();
+      int c = s.nextInt();
     
       System.out.print("Enter forth number :");
-      int d = sc.nextInt();
+      int d = s.nextInt();
 
 
      System.out.println(Math.min(a,Math.min(b,Math.min(c,d))));
